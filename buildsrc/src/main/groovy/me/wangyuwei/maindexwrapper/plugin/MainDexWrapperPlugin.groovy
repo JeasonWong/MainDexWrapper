ad8554db073972fa4a2136cdce532a962ac803f2
巴掌 <350062143@qq.com>
2017-11-23 13:57:35 +0800

    set up structure

---
+++ buildsrc/src/main/groovy/me/wangyuwei/maindexwrapper/plugin/MainDexWrapperPlugin.groovy
@@ -0,0 +1,11 @@
+package me.wangyuwei.maindexwrapper.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project;
+
+public class MainDexWrapperPlugin implements Plugin<Project>{
+    @Override
+    void apply(Project project) {
+       
+    }
+}
